493af6bd5dce2707927216682abeb22628efac89
SamheinDM <SamheinDM@yandex.ru>
2020-10-22 19:14:17 +0300

    init: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let num = n.toString();
+  let result = '';
+
+  if (num[0] === '-') {
+      num = num.substring(1);
+  }
+
+    result = num.split('').reverse().join('');
+
+    return result;
 }
